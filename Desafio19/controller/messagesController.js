9d7d18f5b636f2af605c0fbd7203256c547bc669
Adrian Fernandez <77861592+Adn1217@users.noreply.github.com>
2023-04-02 17:51:45 -0500

    Se inicia implementacion de graphQl para servicios de productos

---
+++ Desafio19/controller/messagesController.js
@@ -0,0 +1,30 @@
+import * as service from '../service/messages.js';
+
+export async function getMsgs(){
+    const allMessages = await service.getMessages();
+    // console.log('Los mensajes son: \n', allMessages);
+    return allMessages;
+}
+
+export async function showMsgs(res) {
+    const allMessages = await service.getMessages();
+    // console.log('Los mensajes son: \n', allMessages);
+    res.render('pages/index', {msgs: allMessages})
+}
+
+export async function doSaveMessage(req, res) {
+    const msg = req.body;
+    const newMsg = await service.saveMessage(msg); 
+    res.send({Guardado: newMsg})
+}
+
+export async function doSaveNormMessage(req, res) {
+    const msg = req.body;
+    const newMsg = await service.saveNormalizedMessage(msg); 
+    res.send({Guardado: newMsg})
+}
+
+export async function getNormMessages(req, res) {
+    const allNormMessages = await service.getNormMessages();
+    return allNormMessages;
+}
