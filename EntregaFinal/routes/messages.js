4061d4dc925c81c810e3bb04453de419ecff4285
Adrian Fernandez <77861592+Adn1217@users.noreply.github.com>
2023-04-19 18:19:17 -0500

    Primer version de Entrega Final

---
+++ EntregaFinal/routes/messages.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import {isLogged} from '../functions.js';
+import * as msgController from '../controller/messagesController.js';
+import {messagesGraphql} from './graphql/messages.js';
+
+const { Router } = express;
+export const mensajes = new Router();
+
+mensajes.use('/', isLogged);
+
+mensajes.get('/', async (req, res) => {
+    msgController.showMsgs(res);
+    // res.send({msgs: allMessages})
+})
+
+mensajes.post('/', (req, res) => {
+    msgController.doSaveMessage(req, res);
+})
+
+mensajes.post('/normalized', (req, res) => {
+    msgController.doSaveNormMessage(req, res);
+})
+
+mensajes.post('/graphql', messagesGraphql);
+mensajes.use('/graphql', messagesGraphql);
